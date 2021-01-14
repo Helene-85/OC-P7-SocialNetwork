a7da5c049e0f63a687ae6a8954d5cff32612a4f8
Helene-mb <bunel.h85@outlook.fr>
2021-01-14 11:55:03 +0100

    Installer et mise en place d'Expess Rate Limit pour prévenir les attaques de force brute

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const rateLimit = require("express-rate-limit"); // Contre les attaques de force brute
 
 // Importation des logiques métier pour les routes
 const userController = require("../controllers/user");
@@ -7,10 +8,15 @@ const userController = require("../controllers/user");
 // Importation du middleware verifyPassword pour contrôler la complexité du mot de passe
 const verifyPassword = require("../middleware/verifyPassword");
 
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes de test
+    max: 5 // 5 essais maximum par adresse IP
+  });
+
 // Route POST pour l'inscription d'un utilisateur
 router.post("/signup", verifyPassword, userController.signup);
 
 // Route POST pour la connexion d'un utilisateur
-router.post("/login", userController.login);
+router.post("/login", limiter, userController.login);
 
 module.exports = router;
