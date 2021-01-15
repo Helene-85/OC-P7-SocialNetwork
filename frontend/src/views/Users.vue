d2e5993db6b661b5ae76f025e79ef451d521b7a4
Helene-mb <bunel.h85@outlook.fr>
2021-01-15 11:08:55 +0100

    Affichage des users sur la page membres

--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -3,16 +3,41 @@
     <h1 class="mt-12 text-2xl  text-center text-white">
     Découvrez les membres de <span class="uppercase text-green-500">Groupo'link</span>
     </h1>
-    <user></user>
+    <user v-for="user in users" :key="user.id"></user>
   </div>
 </template>
 
 <script>
+
 import User from '@/components/User.vue'
+import axios from 'axios'
+
 export default {
   name: 'users',
+  data() {
+    user: "",
+    users: [],
+  },
   components: {
     user: User
+  },
+  methods: {
+    login() {
+      const payload = {
+        pseudo:this.pseudo,
+        email: this.email,
+        profilPic: this.profilPic,
+        isAdmin: this.isAdmin
+      }
+      axios
+        .get('http://localhost:3000/api/users', payload)
+        .then(res => {
+          (this.users = res.data.users)
+        })
+        .catch(() => {
+          console.log('Impossible d\'afficher les users')
+        })
+    }
   }
 }
 </script>
